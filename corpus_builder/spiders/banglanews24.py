e1d2124611f7b1fbf5f2631427819c0e56e1856b
Aniruddha Adhikary <aniruddha@adhikary.net>
2016-06-29 12:16:42 +0600

    spider: banglanews24 with category and pagination

--- corpus_builder/spiders/banglanews24.py
+++ corpus_builder/spiders/banglanews24.py
@@ -6,9 +6,12 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from corpus_builder.items import TextEntry
 
-# Note: The spider only works for the "Printed Edition", for now.
-# Note: This spider works only on Page #1, pagination is under development
-
+# crawl page 1 to 5 of each category and subcategory
+#
+# scrapy crawl banglanews24 -a start_page=1
+# scrapy crawl banglanews24 -a start_page=1 -a end_page=5
+# scrapy crawl banglanews24 -a start_page=1 -a end_page=5 -a category=7
+# scrapy crawl banglanews24 -a start_page=1 -a end_page=5 -a category=17 -a subcategory=5
 
 class Banglanews24Spider(CrawlSpider):
     name = "banglanews24"
@@ -32,14 +35,21 @@ class Banglanews24Spider(CrawlSpider):
         if not (start_page or end_page):
             raise ValueError("start_page, end_page must be provided as arguments")
 
-        self.start_page = start_page
-        self.end_page = end_page
+        self.start_page = int(start_page)
+        if end_page:
+            self.end_page = int(end_page)
+        else:
+            self.end_page = start_page
+
+        self.category = None
+        self.subcategory = None
+
         if category:
             self.category = int(category)
-        else:
-            self.category = None
-        if subcategory:
-            self.subcategory = int(subcategory)
+            if subcategory:
+                self.subcategory = int(subcategory)
+        elif subcategory:
+            raise ValueError("subcategory must be accompanied by category")
 
     	super(Banglanews24Spider, self).__init__(*a, **kw)
 
@@ -48,14 +58,26 @@ class Banglanews24Spider(CrawlSpider):
             callback=self.start_categorized_requests)
 
     def start_categorized_requests(self, response):
+        categories = []
+
         if not self.category:
             categories = list(set(response.css('.navbar-nav a::attr("href")').re('.*/category/.*') + \
                 response.css('.navbar-nav a::attr("href")').re('.*/subcategory/.*')))
-
-            for category_url in categories:
-            	yield scrapy.Request(category_url, callback=self.start_news_requests)
         else:
-            raise NotImplementedError("category-based crawling not implemented yet")
+            if not self.subcategory:
+                categories = response.css('.navbar-nav a::attr("href")').re('.*/category/.*/{0}/?$'.format(
+                    self.category))
+            else:
+                categories = response.css('.navbar-nav a::attr("href")').re('*/subcategory/.*/{0}/{1}/?$'.format(
+                    self.category, self.subcategory))
+
+        if not categories:
+            raise ValueError('invalid category or subcategory id')
+
+        for category_url in categories:
+            for page_number in range(self.start_page, self.end_page+1):
+                yield scrapy.Request(category_url+'?page={0}'.format(page_number),
+                    callback=self.start_news_requests)
 
     def start_news_requests(self, response):
         news_links = list(set(response.css('a::attr("href")').re(".*\/news\/.*")))
