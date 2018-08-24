373410136831c8e336921270b5e8cbd2da704826
Aniruddha Adhikary <aniruddha@adhikary.net>
2018-08-24 19:23:51 +0600

    add somewherein blog

--- corpus_builder/pipelines.py
+++ corpus_builder/pipelines.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+from scrapy.exceptions import DropItem
+
+
+class StripPipeline(object):
+    def process_item(self, item, spider):
+        if item['body']:
+            item['body'] = item['body'].strip()
+            return item
+        else:
+            raise DropItem("Empty Body")
---
+++ corpus_builder/spiders/public_blog/somewherein.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule
+
+from corpus_builder.templates.spider import CommonSpider
+
+
+class SomewhereInSpider(CommonSpider):
+    name = 'somewherein'
+    allowed_domains = ['www.somewhereinblog.net', 'somewhereinblog.net']
+    base_url = 'http://www.somewhereinblog.net/'
+    start_request_url = base_url
+
+    content_body = {
+        'css': '.blog-content::text'
+    }
+
+    rules = (
+        Rule(LinkExtractor(
+            restrict_css='h2.post-title'
+        ),
+            callback='parse_content'),
+    )
+
+    allowed_configurations = [
+        ['start_page'],
+        ['start_page', 'end_page']
+    ]
+
+    def request_index(self, response):
+        for page in range(self.start_page - 1, self.end_page ):
+            yield scrapy.Request(self.base_url + 'page/{page}'.format(page=page * 15))
